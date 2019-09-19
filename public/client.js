a35741da468aafd75bca794b9d0e878bc9ab0607
Rick Gillis <rickg@yuco.com>
2019-09-19 15:14:34 -0700

    Working video encoding

--- public/client.js
+++ public/client.js
@@ -42,6 +42,8 @@ nameForm.onsubmit = function(event) {
   nameInput.focus();
 };*/
 
+function clamp(a, b, c){return Math.max(b,Math.min(c,a));}
+
 
 
 /* * * * * * * * * * *
@@ -50,46 +52,116 @@ nameForm.onsubmit = function(event) {
 
 * * * * * * * * * * */
 
-var x = 0, y = 0;
-
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
-      ctx.font = "30px Arial";
-
-const video = document.getElementById("video");
-
-const maskCanvas = document.getElementById("canvasMask");
-const maskCtx = maskCanvas.getContext('2d');
-const maskVideo = document.getElementById("videoMask");
-
-const w = canvas.width, h = canvas.height;
+class Scene {
   
-
-var draw = function(){ 
-  ctx.clearRect(0, 0, w, h);
-  
-  maskCtx.drawImage(maskVideo, x, y); 
-  
-  ctx.drawImage(video, x, y);
-  ctx.fillText("Hi there", 500, 200);
-  
-  let frame = maskCtx.getImageData(0, 0, w, h);
-  let l = frame.data.length / 4;
-  
-  let cols = ctx.getImageData(0, 0, w, h);
-  
-  for (let i = 0; i < l; i++) {
-    let r = frame.data[i * 4 + 0];
-    let g = frame.data[i * 4 + 1];
-    let b = frame.data[i * 4 + 2];
-    if (g > 250 && r > 250 && b > 250)
-      cols.data[i * 4 + 3] = 0;
+  constructor(width, height){
+    this.width = width;
+    this.height = height;
+    
+    this.objects = [];
   }
   
-  ctx.putImageData(cols, 0, 0);
+  register(object){
+    if(!(object instanceof SceneObject)) return;
+    
+    let objects = this.objects;
+    let index = objects.indexOf(object);
+    if(index > -1) return;
+    
+    objects.push(object);
+    console.log("Added : " + object.name);
+  }
+  
+  unregister(object){
+    let index = this.objects.indexOf(object);
+    if(index < 0)
+        return;
+    
+    this.objects.splice(index, 1);
+  }
+}
+
+class SceneObject { 
+  
+  constructor(x, y, scene){
+    this.x = x;
+    this.y = y;
+    
+    this.scene = scene;
+    this.init();
+  }
+  
+  init() {
+    let scene = this.scene;
+    scene.register(this);
+  }
+  
+  render(x, y){
+    ellipse(x, y, 5, 5);
+  }
+  
 }
 
 
+class Camera extends SceneObject {
+  
+  constructor(x, y, width, height, scene){
+    super(x, y, scene);
+    
+    this.width = width;
+    this.height = height;
+    
+  }
+  
+  render(canvas){
+    let scene = this.scene;
+    let objects = scene.objects;
+    
+    if(!objects)
+      return;
+    
+    for(let i = 0; i < objects.length; i++){
+      let obj = objects[i];
+      if(obj != this){
+        let dx = (obj.x - this.x);
+        let dy = (obj.y - this.y);
+        
+        
+        obj.render(dx, dy);
+      }
+    }
+  }
+  
+}
+
+class SceneImage extends SceneObject {
+  
+  constructor(x, y, width, height, image, scene){
+    super(x, y, scene);
+    
+    if(width > 0 && height > 0){
+      this.width = width;
+      this.height = height;
+    }
+    
+    this.image = image;
+  }
+  
+  render(x, y){
+    let img = this.image;
+    if(!img)
+      return;
+    
+    let w = this.width; let h = this.height;
+    
+    if(w && h){
+      image(img, x, y, w, h);
+      return;
+    }
+    
+    image(img, x, y);
+  }
+}
 
 /* * * * * * * * * * *
 
@@ -97,50 +169,13 @@ var draw = function(){
 
 * * * * * * * * * * */
 
-
 var capturing = false;
 var captureArr = [];
-var captureTime = 8;
+var captureTime = 10;
 
-var form = document.getElementById('file-form');
-var fileSelect = document.getElementById('file-select');
-var uploadButton = document.getElementById('upload-button');
-
-form.onsubmit = function(event) {
-  event.preventDefault();
- 
-  // Update button text.
-  uploadButton.innerHTML = 'Uploading...';
- 
-  var files = fileSelect.files;
-  var formData = new FormData();
-  
-  for (var i = 0; i < files.length; i++) {
-   var file = files[i];
- 
-   // Check the file type.
-   if (!file.type.match('image.*')) {
-     continue;
-   }
- 
-   // Add the file to the request.
-  formData.append('photos[]', file, file.name);
- }
-  
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/sendImages', true);
-  
-  xhr.onload = function () {
-  if (xhr.status === 200) {
-    // File(s) uploaded.
-    uploadButton.innerHTML = 'Upload';
-  } else {
-    alert('An error occurred!');
-  }
-};
-  
-  xhr.send(formData);
-}
+const framerate = 24;
+var frames = 0;
+var totalFrames = framerate * captureTime;
 
 
 const saveButton = document.getElementById("saveButton");
@@ -148,36 +183,100 @@ const saveButton = document.getElementById("saveButton");
 
         if(capturing)
           return;
-        
-        capturing = true;
-        captureArr = [];
+       
+        capturing = true;    
+        saveFrames('frame', 'jpg', captureTime, framerate, function(arr) {
+          frames = 0; totalFrames = arr.length;
+          captureArr = arr;
+
+          for(let i = 0; i < totalFrames; i++)
+            sendFrame(arr[i].imageData);
+        });
+
         
       });
 
-var frameCount = -1;
+// CONTROLS
 
-var framerate = 30;
+var l = false, r = false, u = false, d = false;
+window.addEventListener("keydown", function(e){
+  let key = e.key;
+  
+  if(key == "a") l = true;
+  if(key == "d") r = true;
+  if(key == "s") d = true;
+  if(key == "w") u = true;
+});
 
-var framesCompleted = 0;
-var totalFrames = 0;
+window.addEventListener("keyup", function(e){
+  let key = e.key;
+  
+  if(key == "a") l = false;
+  if(key == "d") r = false;
+  if(key == "s") d = false;
+  if(key == "w") u = false;
+});
+
+const scene = new Scene(1080, 1080); console.log("Scene created at " + scene.width + "x" + scene.height);
+const camera = new Camera(0, 0, 640, 640, scene);
+var canvas;
+
+var bg;
+var ellipses = [];
+
+const video = document.getElementById("video");
+var vid;
+
+function setup(){
+  canvas = createCanvas(camera.width, camera.height);
+  vid = createVideo([video.src], function(){ vid.loop(); bg = new SceneImage(5, 5, -1, -1, vid, scene);});
+  vid.hide();
+  
+  stroke(255);
+  frameRate(24);
+  
+  for(let i = 0; i < 50; i++){
+    let e = new SceneObject(Math.floor(Math.random() * scene.width), Math.floor(Math.random() * scene.height), scene);
+    ellipses.push(e);
+  }
+}
+
+let y = 100;
+function draw() {
+  background(0);
+  
+  let dx = 0;
+  if(r) dx += 1;
+  if(l) dx -= 1;
+  
+  let dy = 0;
+  if(u) dy -= 1;
+  if(d) dy += 1;
+  
+  camera.x = clamp(camera.x + (dx * 10), 0, scene.width - camera.width); 
+  camera.y = clamp(camera.y + (dy * 10), 0, scene.height - camera.height);
+  
+  camera.render(canvas);
+}
 
 const sendFrame = function(frame){
-  ++frameCount;
+  ++frames;
   
   $.ajax({
     type: 'POST',
     url: '/addFrame',
     data: {
-      png: frame,
-      frame: frameCount
+      jpg: frame,
+      frame: frames
     },
     
     success: function(){
-      ++framesCompleted;
-      console.log("frames uploaded: ", framesCompleted, "/", totalFrames);
+      console.log("frames uploaded: ", frames, "/", totalFrames);
       
-      if(framesCompleted >= totalFrames)
+      if(frames >= captureArr.length-1){
         encode("svs6");
+        capturing = false;
+      }
     },
     error: function(err){
       console.log("error uploading: ", err);
@@ -186,8 +285,14 @@ const sendFrame = function(frame){
   });
 }
 
+var encoded = false;
+
 const encode = function(filename){
   filename = filename || "unnamed";
+
+  if(encoded)
+    return;
+  encoded = true;
   
   $.ajax({
     type: 'POST',
@@ -209,7 +314,7 @@ const encode = function(filename){
 
 * * * * * * * * * * */
 
-var t0 = Date.now();
+/*var t0 = Date.now();
 function update(dt) {
   //x = 500*Math.cos(dt / 500);
   //y = 200*Math.sin(dt / 500);
@@ -240,5 +345,51 @@ function render(){
 
   update(dt);
 }
-render();
+render();*/
 
+
+/* * * * * * * * * * *
+
+      Drawing
+
+* * * * * * * * * * */
+
+/*var x = 0, y = 0;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+      ctx.font = "30px Arial";
+
+const video = document.getElementById("video");
+      video.play();
+
+const maskCanvas = document.getElementById("canvasMask");
+const maskCtx = maskCanvas.getContext('2d');
+const maskVideo = document.getElementById("videoMask");
+
+const w = canvas.width, h = canvas.height;
+  
+
+var draw = function(){ 
+  ctx.clearRect(0, 0, w, h);
+  
+  maskCtx.drawImage(maskVideo, x, y); 
+  
+  ctx.drawImage(video, x, y);
+  ctx.fillText("Hi there", 500, 200);
+  
+  let frame = maskCtx.getImageData(0, 0, w, h);
+  let l = frame.data.length / 4;
+  
+  let cols = ctx.getImageData(0, 0, w, h);
+  
+  for (let i = 0; i < l; i++) {
+    let r = frame.data[i * 4 + 0];
+    let g = frame.data[i * 4 + 1];
+    let b = frame.data[i * 4 + 2];
+    if (g > 250 && r > 250 && b > 250)
+      cols.data[i * 4 + 3] = 0;
+  }
+  
+  ctx.putImageData(cols, 0, 0);
+}*/
--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const ffmpeg = require('fluent-ffmpeg')
       ffmpeg.setFfmpegPath(ffmpegPath);
 
 
-
 const app = express();
 
 var tempDir = tmp.dirSync({ unsafeCleanup: true });
@@ -30,8 +29,9 @@ var outputDir = __dirname + "/output";
 
 // Using
 
-app.use(express.static('public'));
-app.use(express.static('output'));
+app.use(express.static('public'))
+   .use(express.static('lib'))
+   .use(express.static('output'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
@@ -51,7 +51,7 @@ app.use(function(req, res, next) {
 
 app.get('/', function(request, response) {
   console.log("hi");	
-  response.sendFile(__dirname + 'awdawdw.html');
+  response.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/view', (req, res) => {
@@ -74,11 +74,11 @@ app.post('/submit-name', (req, res) => {
 });
 
 app.post('/addFrame', (req, res) => {
-  console.log("received frame" + req.body.frame);
-
-  let frame = req.body.png.replace(/^data:image\/png;base64,/, "");
-  let fName = sprintf('frame-%03d.png', parseInt(req.body.frame));
+  let frame = req.body.jpg.replace(/^data:image\/(png|jpg);base64,/, "");
+  let fName = sprintf('frame-%03d.jpg', parseInt(req.body.frame));
   let dir = tempDir.name + "/" + fName;
+
+  console.log('received frame: ' + fName);
   
   fs.writeFile(dir, frame, 'base64', function(err){
     if(err)
@@ -90,43 +90,34 @@ app.post('/addFrame', (req, res) => {
 
 app.post('/encode', (req, res) => {
   let oldTemp = tempDir;
+  console.log(outputDir + "/" + req.body.path + ".mp4");
   
-  console.log(req.body);
-  
-  console.log("Begin render...");
-  var ffmpeg = cp.spawn('ffmpeg', [ '-y',
-    '-framerate', '30',
-    '-start_number', '0',
-    '-i', 'frame-%03d.png',
-    '-refs', '5',
-    '-c:v', 'libx264',
-    '-crf', '18',
-    '-pix_fmt', 'yuv420p',
-    '-s', '640x480',
-    '-b:v', '1024',
-    '-b:a', '128k',
-                                   
-    outputDir + "/" + req.body.path + ".mp4"
-  ], {
-    cwd: oldTemp.name,
-    stdio: 'inherit'
-  });
-  ffmpeg.on('close', function(code) {
-    console.log('Finished rendering video. You can find it at: ' + outputDir + '/' + req.body.path + '.mp4');
-    oldTemp.removeCallback();
-  });
-  
+  var proc = new ffmpeg()
+      .input(tempDir.name + '/frame-%03d.jpg')
+      .fps(24)
+      .outputOptions([
+        '-framerate 24',
+        '-start_number 0',
+        '-refs 5',
+        '-c:v libx264',
+        '-crf 18',
+        '-b:v 1024',
+        '-b:a 128k'
+      ])
+      .on('start', function(){
+        console.log("Begin render!");
+      })
+      .on('error', function(err) {
+        console.log('An error occurred: ' + err.message);
+      })
+      .on('end', function() {
+        console.log('End render!');
+        oldTemp.removeCallback();
+      })
+      .save(outputDir + '/' + req.body.path + '.mp4')
+
   tempDir = tmp.dirSync({unsafeCleanup: true});
   res.end();
-});
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
 });
 
 // listen for requests :)
