3edc871fa271dc4f1ab76d62a76d0bd01f70645a
Rick Gillis <rickg@yuco.com>
2019-10-22 18:30:42 -0700

    Removed process

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let DEEP_LINK_ID = "";
-const S3URL =  process.env.S3_URL || 'https://social-sharing-install.s3-us-west-2.amazonaws.com/tec-demo';
+const S3URL = 'https://social-sharing-install.s3-us-west-2.amazonaws.com/tec-demo';
 
 // Exec on page load - Adding button action assignments.
 $(document).ready(() => {
