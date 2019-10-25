2fc92b142e0221e1ec677f391571df32d96a4c3f
Rick Gillis <rickg@yuco.com>
2019-10-25 10:21:31 -0700

    no way

--- locust/test.py
+++ locust/test.py
@@ -1,6 +1,4 @@
-from locust import HttpLocust, TaskSet, TaskSequence, task, seq_task
-
-import pickle
+from locust import HttpLocust, TaskSet, TaskSequence, task, seq_task, events
 
 import os
 import math
@@ -8,9 +6,9 @@ import random
 import json
 import base64
 
-loc = 'locust/' # declare subfolder
+loc = 'locust/' # declare subfolder, IF CONTAINING FOLDER CHANGES, MAKE SURE TO UPDATE THIS
 
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ".", "*", "-", "@"  ]
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ".", "-", "@"  ]
 
 # grab images from local folder
 frames = [];
@@ -27,11 +25,17 @@ class Process(TaskSequence):
     lineA = ""
     lineB = ""
 
+    output = ""
+
+    key = "false"
+    link = ""
+
     # your task
     @seq_task(1)
     @task(1)
     def index(self):
         print("open")
+
         self.client.get('/')
         pass
 
@@ -40,6 +44,11 @@ class Process(TaskSequence):
     def addFrames(self):
         print("add frames")
         res = self.client.post('/encoder/addFrames', data=json.dumps({ "frames" : frames}), headers={'content-type': 'application/json'}, catch_response=True)
+
+        if res.status_code == 200 :
+            res.success()
+        else :
+            res.failure("fail add frames")
         pass
 
     @seq_task(3)
@@ -58,51 +67,81 @@ class Process(TaskSequence):
 
         self.lineA = alt
         self.lineB = blt
-
-        print(alt + ",  " + blt)
+        pass
 
     @seq_task(4)
+    def cacheKey(self):
+        s3 = "/output/" + self.lineA + "~" + self.lineB + ".mp4"
+
+
+        print("cache key")
+        res = self.client.post('/aws/s3cacheKey',  data=json.dumps({ "s3Key" : s3 }), headers={'content-type': 'application/json'}, catch_response=True)
+
+        if res.status_code == 200 :
+            res.success()
+        else :
+            res.failure("fail cache key")
+
+        self.key = res.content.decode('utf-8')
+        print(self.key)
+
+        pass
+
+    @seq_task(5)
+    def deeplink(self):
+        v = "/output/" + self.lineA + "~" + self.lineB + ".mp4"
+        p = "/output/" + self.lineA + "~" + self.lineB + ".jpg"
+
+        print("cache key")
+        res = self.client.post('/aws/deepLink',  data=json.dumps({ "videoFilePath" : v, "imageFilePath" : p }), headers={'content-type': 'application/json'}, catch_response=True)
+
+        if res.status_code == 200 :
+            res.success()
+            self.link = res.content.decode('utf-8')
+        else :
+            res.failure("fail deeplink")
+
+        pass
+
+    @seq_task(6)
     def screenshot(self):
         print("screenshot")
-        self.client.post('/encoder/screenshot',  data=json.dumps({ "fileName" : self.lineA + "~" + self.lineB + ".jpg", "dat" : frames[83].get("dat")}), headers={'content-type': 'application/json'}, catch_response=True)
+        res = self.client.post('/encoder/screenshot',  data=json.dumps({ "fileName" : self.lineA + "~" + self.lineB + ".jpg", "dat" : frames[83].get("dat")}), headers={'content-type': 'application/json'}, catch_response=True)
+        
+        if res.status_code == 200 :
+            res.success()
+        else :
+            res.failure("fail screenshot")
         pass
 
-    
-
-    @seq_task(4)
+    @seq_task(7)
     def encode(self):
         print("encode")
-        self.client.post('/encoder/encode', data=json.dumps( { "path" : self.lineA + '~' + self.lineB } ), headers={'content-type': 'application/json'}, catch_response=True)
+        res = self.client.post('/encoder/encode', data=json.dumps( { "path" : self.lineA + '~' + self.lineB } ), headers={'content-type': 'application/json'}, catch_response=True)
+        
+        if res.status_code == 200 :
+            res.success()
+        else :
+            res.failure("fail encode")
+
+        self.output = res.content.decode('utf-8')
         pass
-
-
-#class AWSTasks(TaskSet):
 #
-#    @task
-#    def index():
-#        print("open")
-#        self.client.get('/')
-#
-   # @task
-   # def deeplink():
-   #     print("deeplink")
-   #     self.client.post('/aws/deepLink')
-#
-   # def processId():
-   #     print("process id")
-   #     self.client.post('/aws/processId')
-#
-   # def upload():
-   #     print("upload")
-   #     self.client.post('/aws/s3upload')
-#
-   # def cacheKey:
-   #     print("cache key")
-   #     self.client.post('/aws/s3cacheKey')
+    @seq_task(8)
+    def upload(self):
+        print("upload")
+        res = self.client.post('/aws/s3upload', data=json.dumps( { "videoFilePath": self.output, "imageFilePath" :  self.output.replace("mp4", "jpg", 1) } ), headers={'content-type': 'application/json'}, catch_response=True)
+        
+        if res.status_code == 200 :
+            res.success()
+        else :
+            res.failure("fail upload")
+        
+        pass
 
 
 # task runner
 class LocustRunner(HttpLocust): 
     task_set = Process # add your set to the task runner
-    min_wait = 0
-    max_wait = 5000
+    min_wait = 1000
+    max_wait = 15000
